92e775e76913f3dc8aecd0c59a85cd995e874aa6
Gerry Mc Donnell <irishbloke@gmail.com>
2019-04-06 13:58:21 +0100

    added state

--- src/App.js
+++ src/App.js
@@ -4,7 +4,32 @@ import './App.css';
 import Todos from './components/Todos';
 
 class App extends Component {
+
+  state={
+    todos:[
+      {
+        id:1,
+        title:'gdfg 1 fdgdfgfdg',
+        completed:false
+      },
+      {
+        id:2,
+        title:'gdfg 2 fdgdfgfdg',
+        completed:false
+      },
+      {
+        id:3,
+        title:'gdfg 3 fdgdfgfdg',
+        completed:false
+      }
+    ]
+  }
+
+
   render() {
+    
+    console.log(this.state.todos)
+
     return (
       <div className="App">
         <h1> App</h1>
