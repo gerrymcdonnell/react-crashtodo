d74d9b6f89787663c4b977238155593092792cad
Gerry Mc Donnell <irishbloke@gmail.com>
2019-04-06 14:09:49 +0100

    display todos

--- src/App.js
+++ src/App.js
@@ -5,35 +5,37 @@ import Todos from './components/Todos';
 
 class App extends Component {
 
-  state={
-    todos:[
+  state = {
+    todos: [
       {
-        id:1,
-        title:'gdfg 1 fdgdfgfdg',
-        completed:false
+        id: 1,
+        title: 'gdfg 1 fdgdfgfdg',
+        completed: false
       },
       {
-        id:2,
-        title:'gdfg 2 fdgdfgfdg',
-        completed:false
+        id: 2,
+        title: 'gdfg 2 fdgdfgfdg',
+        completed: false
       },
       {
-        id:3,
-        title:'gdfg 3 fdgdfgfdg',
-        completed:false
+        id: 3,
+        title: 'gdfg 3 fdgdfgfdg',
+        completed: false
       }
     ]
   }
 
 
   render() {
-    
+
+    //print out the state
     console.log(this.state.todos)
 
     return (
       <div className="App">
-        <h1> App</h1>
-        <Todos/>
+        <h1> My App</h1>
+        {/* {pass todos to component} */}
+        <Todos myprop1="prop1" todos={this.state.todos} />
       </div>
     );
   }
