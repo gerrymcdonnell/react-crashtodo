7e5ac7cd24d012947d9a8d0419c817c0be48b9f2
Gerry Mc Donnell <irishbloke@gmail.com>
2019-04-07 14:55:18 +0100

    added bootstrap 4

--- src/layout/header.js
+++ src/layout/header.js
@@ -1,10 +1,13 @@
 //functional component
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 function Header() {
     return (
         <header style={headerStyle}>
             <h1>TodoList</h1>
+            <Link to="/">Home </Link>|
+            <Link to="/about">About </Link>
         </header>
     )
 }
